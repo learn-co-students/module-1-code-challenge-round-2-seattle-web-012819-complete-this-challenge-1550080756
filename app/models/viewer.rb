32527ec3923bc5a4175857a3d0c7750256530315
github username <github email address>
2019-02-13 11:04:02 -0800

    Second Attempt

--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,21 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+  def queue_items
+    QueueItem.all.select{|x| x.viewer == self}
+  end
+
+  def queue_movies
+    self.queue_items.map{|x| x.movie}
+  end
+
+  def add_movie_to_queue(movie)
+    QueueItem.new(self, movie)
+  end
+
+  def rate_movie(movie, rating)
+    QueueItem.new(self, movie, rating)
+  end
+
 end
