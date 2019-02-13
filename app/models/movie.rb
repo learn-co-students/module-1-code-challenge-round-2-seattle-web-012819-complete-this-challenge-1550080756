a90cb5a6b855cfea899a0232ce34eefd35819c64
github username <github email address>
2019-02-13 11:10:24 -0800

    Finished the second code challenge

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,37 @@ class Movie
     @@all
   end
 
-end
+  def queue_items
+    QueueItem.all.select do |queue|
+      queue.movie == self
+    end
+  end
+
+  def viewers
+    self.queue_items.map do |queue|
+      queue.viewer
+    end
+  end
+
+  def average_rating
+    total_rating = 0
+    count_rating = 0
+    self.queue_items.each do |movie|
+      total_rating += movie.rating
+      count_rating += 1
+    end
+    total_rating / count_rating.to_f
+  end
+
+  def self.highest_rated
+    highest_ave_rating =0
+    highest_rated_movie = ""
+    QueueItem.all.each do |queue_items|
+      if queue_items.movie.average_rating > highest_ave_rating
+        highest_ave_rating = queue_items.movie.average_rating
+        highest_rated_movie = queue_items.movie
+      end
+    end
+    highest_rated_movie
+  end
+end #end of class
--- tools/console.rb
+++ tools/console.rb
@@ -7,6 +7,19 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+m1 = Movie.new("StarWars")
+m2 = Movie.new("Ironman")
+m3 = Movie.new("Spiderman")
+
+v1 = Viewer.new("Rob007")
+v2 = Viewer.new("Mike008")
+v3 = Viewer.new("Galaxy009")
+
+q1 = QueueItem.new(m1, v2, 4.0)
+q2 = QueueItem.new(m2, v1, 3.0)
+q3 = QueueItem.new(m1, v3, 5.0)
+q4 = QueueItem.new(m3, v1, 4.0)
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
