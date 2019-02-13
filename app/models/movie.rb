4fae744d59d26d51d3cfdab2c3b752a2710c99a8
Jessica Poe <jesspoe00@gmail.com>
2019-02-13 11:03:24 -0800

    Second code challenge

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,35 @@ class Movie
     @@all
   end
 
-end
+  def queue_items
+    QueueItem.all.select do |item|
+      item.movie == self 
+    end 
+  end 
+
+  def viewers 
+    queue_items.map do |movieq|
+      movieq.viewer
+    end 
+  end 
+
+  def average_rating 
+    count = 0 
+    total = 0
+    queue_items.each do |item|
+      count +=1
+      total += item.rating
+    end 
+    avg = total/count 
+  end 
+
+  def self.highest_rated 
+    highest = []
+    @@all.select do |movie| 
+      highest << movie.average_rating
+      binding.pry 
+    end 
+    highest
+  end 
+
+end # End Movie Class
--- app/models/queue_item.rb
+++ app/models/queue_item.rb
@@ -1,4 +1,20 @@
 class QueueItem
+    attr_accessor :viewer, :movie, :rating
+
+    @@all = []
+  
+    def initialize(viewer, movie, rating = nil)
+      @viewer = viewer 
+      @movie = movie 
+      @rating = rating 
+      self.class.all << self
+    end
+  
+    def self.all
+      @@all
+    end
+    
+  
 
 
-end
+end # End QueueItem class
